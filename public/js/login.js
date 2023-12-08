94bb070d301f2e63657129fc2cc59a5b6368543e
tfkjosh <140848241+tfkjosh@users.noreply.github.com>
2023-12-07 22:31:20 -0600

    Update login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,13 +1,10 @@
-// Already registered user handler
 const loginFormHandler = async (event) => {
     event.preventDefault();
   
-    // Collect values from the login form
     const email = document.querySelector("#email-login").value.trim();
     const password = document.querySelector("#password-login").value.trim();
   
     if (email && password) {
-      // Send a POST request to the API endpoint
       const response = await fetch("/api/users/login", {
         method: "POST",
         body: JSON.stringify({ email, password }),
@@ -15,15 +12,13 @@ const loginFormHandler = async (event) => {
       });
   
       if (response.ok) {
-        // If successful, redirect the browser to the home page
         document.location.replace("/");
       } else {
         alert(response.statusText);
       }
     }
   };
-  
-  // Sign up handler
+
   const signupFormHandler = async (event) => {
     event.preventDefault();
   
@@ -46,7 +41,6 @@ const loginFormHandler = async (event) => {
     }
   };
   
-  // Event Listeners
   document
     .querySelector(".login-form")
     .addEventListener("submit", loginFormHandler);
