c09f55e566f81b1d8e08b54ae5973e266ae129f6
tfkjosh <140848241+tfkjosh@users.noreply.github.com>
2023-12-07 22:15:56 -0600

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,8 @@
-// Dotenv import
 require("dotenv").config();
 
 // Sequelize import
 const Sequelize = require("sequelize");
 
-// Allows for environmental variables to be used
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
