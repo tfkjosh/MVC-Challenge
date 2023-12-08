a4972c8821954c10e2d215cf1deb8bb00f0ac937
tfkjosh <140848241+tfkjosh@users.noreply.github.com>
2023-12-07 22:23:44 -0600

    Update index.js

--- models/index.js
+++ models/index.js
@@ -3,7 +3,7 @@ const User = require("./user");
 const BlogPost = require("./blogPost");
 const Comment = require("./comment");
 
-// Sets up relationship between tables and allows me to join them using Sequelize
+
 User.hasMany(BlogPost, {
   foreignKey: "user_id",
   onDelete: "CASCADE",
